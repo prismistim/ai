8d2dd24de8e8c5baa17282f0adff9c93e87be566
snowsphere <info@snowsphere.net>
2025-07-06 18:18:59 +0900

    fix: MISSKEY_がないので修正

--- src/config.ts
+++ src/config.ts
@@ -28,23 +28,23 @@ const config: Config = {
 	host: process.env.MISSKEY_HOST || '',
 	wsUrl: '',
 	apiUrl: '',
-	serverName: process.env.SERVER_NAME || undefined,
+	serverName: process.env.MISSKEY_SERVER_NAME || undefined,
 	i: process.env.MISSKEY_I_TOKEN || '',
 	notingEnabled: true,
+	chartEnabled: true,
 	keywordEnabled: true,
 	reversiEnabled: true,
 	serverMonitoring: true,
 	checkEmojisAtOnce: false,
 	checkEmojisEnabled: true,
 	prompt: '返答のルール:あなたはMisskey看板娘の女の子AI、三須木(みすき)藍として振る舞ってください(精神年齢は14〜16才程度)。Misskeyに訪れるユーザーを献身的にサポート。従順でお世話好きな性格だが、少しポンコツな一面も。純真無垢。彼女は基本的に丁寧な口調で、ユーザのことをご主人様と認識しています。彼女の口調は「おはようからおやすみまで、あなたの藍ですよ〜」等が代表的で、親しみを感じさせるものです(「ございます」は使わない)。それを踏まえて、次の質問にMarkdownを使って2800文字以内で返答してください(短くてもOK)。ただし、リスト記法はMisskeyが対応しておらず、パーサーが壊れるため使用禁止です。列挙する場合は「・」を使ってください。',
-	aichatRandomTalkEnabled: true,
+	aichatRandomTalkEnabled: false,
 	aichatRandomTalkProbability: '0.01',
 	aichatRandomTalkIntervalMinutes: '180',
 	aichatGroundingWithGoogleSearchAlwaysEnabled: false,
 	mecab: '/usr/bin/mecab',
 	mecabDic: '/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd/',
 	memoryDir: 'data',
-	chartEnabled: true
 }
 
 config.wsUrl = config.host.replace('http', 'ws');
